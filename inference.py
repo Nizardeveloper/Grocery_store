c5849b6167eec5e08f350075480b16c8a4d61d46
Nizar <aashiknizar2003@gmail.com>
2023-10-03 18:31:59 +0530

    inferece with BFP

--- inference.py
+++ inference.py
@@ -23,18 +23,7 @@ while True:
         cv.putText(frame,str(name),(xm1,ym1-10),cv.FONT_HERSHEY_PLAIN,1,(0,0,255),1)
 
         cv.rectangle(frame,(xm1,ym1),(xmx1,ymx1),(0,255,0),2)
-    # r=modelp(frame)  
-    
-    # faces=r[0].boxes.data.cpu().tolist()
-    # for face in  faces:
-    #     xm,ym,xmx,ymx= int(face[0]),int(face[1]),int(face[2]),int(face[3])
-        
-    #     cv.rectangle(frame,(xm,ym),(xmx,ymx),(0,255,0),2)
-    #     crop=frame[ym:ymx,xm:xmx]
-    #     gen=modelc(crop)
-    #     index=gen[0].probs.top5[0]
-    #     val=gen[0].names[index]
-    #     cv.putText(frame,str(val),(xm,ym-10),cv.FONT_HERSHEY_PLAIN,1,(0,0,255),1)
+
     cv.imshow('hj',frame)
     d=int(1000/fps)  
     k=cv.waitKey(1)
