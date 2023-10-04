e89cfd05dc74a1418a28c44ee9d9800fa0c5c4e0
Nizar <aashiknizar2003@gmail.com>
2023-10-04 16:15:14 +0530

    Revert "Merge branch 'jaya' into Nizar"
    This reverts commit b3090e7ae33d19b2cf5e6fdc365f2c989b2e4d4a, reversing
    changes made to 6ceefbd1c7dfa036ef69a4b65caf0f824bd3ff2a.

--- inference.py
+++ inference.py
@@ -2,45 +2,47 @@ from ultralytics import YOLO
 import cv2 as cv
 import numpy as np
 
-modelp=YOLO('yolov8n.pt')
-modelc=YOLO('Gender_Classification.pt')
+modelp=YOLO(r"d:\JSN\Grocery_store\Work_area\model\BFP_grocery_v2.pt")
+modelf=YOLO(r"D:\JSN\Grocery_store\Work_area\model\Face_detection.pt")
+modelc=YOLO(r"d:\JSN\Grocery_store\Work_area\model\Gender_Classification.pt")
+#cm=
+vid = cv.VideoCapture(r"d:\JSN\Grocery_store\Work_area\testing\samp1.mp4")
 
-# vid = cv.VideoCapture("rtsp://admin:CCTV@123@192.168.29.2:554/Streaming/channels/202")
-
-# fps=vid.get(cv.CAP_PROP_FPS)
+fps=vid.get(cv.CAP_PROP_FPS)
 fcount=0
 while True:
-    vid = cv.VideoCapture("rtsp://admin:CCTV@123@192.168.29.2:554/Streaming/channels/202")
     ret , frame = vid.read()
     if not ret:
         break
     p=modelp(frame)
-    print(p[0].names)
+    print(p[0].names[0])
     person=p[0].boxes.data.cpu().tolist()
     print(person)
     for per in  person:
         name=p[0].names[int(per[-1])]
-        if name == 'person' or name=='handbag':
-
+        if name!='Face':
             xm1,ym1,xmx1,ymx1= int(per[0]),int(per[1]),int(per[2]),int(per[3])
-            
-            cv.putText(frame,str(name),(xmx1,ymx1+10),cv.FONT_HERSHEY_PLAIN,1,(0,0,255),1)
- 
+            name=p[0].names[int(per[-1])]
+            cv.putText(frame,str(name),(xm1,ym1-10),cv.FONT_HERSHEY_PLAIN,1,(0,0,255),1)
+
             cv.rectangle(frame,(xm1,ym1),(xmx1,ymx1),(0,255,0),2)
-    # r=modelp(frame)  
+    r=modelf(frame)  
     
-    # faces=r[0].boxes.data.cpu().tolist()
-    # for face in  faces:
-    #     xm,ym,xmx,ymx= int(face[0]),int(face[1]),int(face[2]),int(face[3])
+    faces=r[0].boxes.data.cpu().tolist()
+    for face in  faces:
+        xm,ym,xmx,ymx= int(face[0]),int(face[1]),int(face[2]),int(face[3])
         
-    #     cv.rectangle(frame,(xm,ym),(xmx,ymx),(0,255,0),2)
-    #     crop=frame[ym:ymx,xm:xmx]
-    #     gen=modelc(crop)
-    #     index=gen[0].probs.top5[0]
-    #     val=gen[0].names[index]
-    #     cv.putText(frame,str(val),(xm,ym-10),cv.FONT_HERSHEY_PLAIN,1,(0,0,255),1)
+        cv.rectangle(frame,(xm,ym),(xmx,ymx),(0,255,0),2)
+        # names=r[0].names[int(face[-1])]
+        # cv.putText(frame,str(names),(xmx,ymx+10),cv.FONT_HERSHEY_PLAIN,1,(0,0,255),1)
+
+        crop=frame[ym:ymx,xm:xmx]
+        gen=modelc(crop)
+        index=gen[0].probs.top5[0]
+        val=gen[0].names[index]
+        cv.putText(frame,str(val),(xmx,ymx+10),cv.FONT_HERSHEY_PLAIN,1,(0,0,255),1)
     cv.imshow('hj',frame)
-    # d=int(1000/fps)  
+    d=int(1000/fps)  
     k=cv.waitKey(1)
     if k!=-1:
         break
