ec52e7c284be7f68e41a30b0f12238ed5fc6e612
Nizar <aashiknizar2003@gmail.com>
2023-10-05 18:07:18 +0530

    re-inferencing

--- inference.py
+++ inference.py
@@ -1,49 +1,9 @@
 from ultralytics import YOLO
-import cv2 as cv
+import cv2
 import numpy as np
 
-modelp=YOLO(r"d:\JSN\Grocery_store\Work_area\model\BFP_grocery_v2.pt")
-modelf=YOLO(r"D:\JSN\Grocery_store\Work_area\model\Face_detection.pt")
-modelc=YOLO(r"d:\JSN\Grocery_store\Work_area\model\Gender_Classification.pt")
-#cm=
-vid = cv.VideoCapture(r"d:\JSN\Grocery_store\Work_area\testing\samp1.mp4")
+person = YOLO("d:\JSN\Grocery_store\Work_area\model\yolov8n.pt")
+bag = YOLO("d:\JSN\Grocery_store\Work_area\model\Bag_Person_100n.pt")
+face = YOLO("d:\JSN\Grocery_store\Work_area\model\yolov8n-face.pt")
+gender = YOLO("d:\JSN\Grocery_store\Work_area\model\Gender_Classification.pt")
 
-fps=vid.get(cv.CAP_PROP_FPS)
-fcount=0
-while True:
-    ret , frame = vid.read()
-    if not ret:
-        break
-    p=modelp(frame)
-    print(p[0].names[0])
-    person=p[0].boxes.data.cpu().tolist()
-    print(person)
-    for per in  person:
-        name=p[0].names[int(per[-1])]
-        if name!='Face':
-            xm1,ym1,xmx1,ymx1= int(per[0]),int(per[1]),int(per[2]),int(per[3])
-            name=p[0].names[int(per[-1])]
-            cv.putText(frame,str(name),(xm1,ym1-10),cv.FONT_HERSHEY_PLAIN,1,(0,0,255),1)
-
-            cv.rectangle(frame,(xm1,ym1),(xmx1,ymx1),(0,255,0),2)
-    r=modelf(frame)  
-    
-    faces=r[0].boxes.data.cpu().tolist()
-    for face in  faces:
-        xm,ym,xmx,ymx= int(face[0]),int(face[1]),int(face[2]),int(face[3])
-        
-        cv.rectangle(frame,(xm,ym),(xmx,ymx),(0,255,0),2)
-        # names=r[0].names[int(face[-1])]
-        # cv.putText(frame,str(names),(xmx,ymx+10),cv.FONT_HERSHEY_PLAIN,1,(0,0,255),1)
-
-        crop=frame[ym:ymx,xm:xmx]
-        gen=modelc(crop)
-        index=gen[0].probs.top5[0]
-        val=gen[0].names[index]
-        cv.putText(frame,str(val),(xmx,ymx+10),cv.FONT_HERSHEY_PLAIN,1,(0,0,255),1)
-    cv.imshow('hj',frame)
-    d=int(1000/fps)  
-    k=cv.waitKey(1)
-    if k!=-1:
-        break
-cv.destroyAllWindows()
